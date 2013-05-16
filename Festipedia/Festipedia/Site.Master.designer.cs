bbd62b62c8d059c9df1482eac70acb9a9d383b9b
Ruben Bielen <ruben.bielen@student.phl.be>
2013-05-16 14:35:04 +0200

    Language design

--- Festipedia/Festipedia/Site.Master.designer.cs
+++ Festipedia/Festipedia/Site.Master.designer.cs
@@ -49,13 +49,13 @@ namespace Festipedia {
         protected global::System.Web.UI.WebControls.LinkButton btnSetEnglish;
         
         /// <summary>
-        /// en control.
+        /// uk control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Image en;
+        protected global::System.Web.UI.WebControls.Image uk;
         
         /// <summary>
         /// FeaturedContent control.
