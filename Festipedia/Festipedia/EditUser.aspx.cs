98dd34a2939e1962292e35fe9f127bea6f940b1b
Rens Kaes <3130545@student.phl.be>
2013-05-16 16:02:57 +0200

    Edit User

---
+++ Festipedia/Festipedia/EditUser.aspx.cs
@@ -0,0 +1,23 @@
+﻿using Festipedia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Festipedia
+{
+    public partial class EditUser : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (var db = new groep15_festivalsContext())
+            {
+                var query = from g in db.GeregistreerdeGebruikers orderby g.gebr_id select g;
+                grdEdit.DataSource = query.ToList();
+                grdEdit.DataBind();
+            }
+        }
+    }
+}
