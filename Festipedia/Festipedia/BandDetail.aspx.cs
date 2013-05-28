f65b93d4c55f2d1985de4d8ead149665f6dfda54
Jonas Windmolders <jonas_windmolders@hotmail.com>
2013-05-28 23:04:12 +0200

    DMP

--- Festipedia/Festipedia/BandDetail.aspx.cs
+++ Festipedia/Festipedia/BandDetail.aspx.cs
@@ -7,11 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace Festipedia
 {
-	public partial class FestiDetail : System.Web.UI.Page
-	{
-		protected void Page_Load(object sender, EventArgs e)
-		{
+    public partial class BandDetail : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
 
-		}
-	}
+        }
+    }
 }
