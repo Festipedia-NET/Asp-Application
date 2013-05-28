2a15c3aafe51c0f635f07b97070ae4a459de7864
Kevin Van Poppel <kevin.vanpoppel@student.phl.be>
2013-05-28 22:17:44 +0200

    FU

--- Festipedia/Festipedia/Admin/adminPage.aspx.cs
+++ Festipedia/Festipedia/Admin/adminPage.aspx.cs
@@ -18,20 +18,41 @@ namespace Festipedia.Admin
                 String[] query = Roles.GetAllRoles();
                 RoleList.DataSource = query.ToList();
                 RoleList.DataBind();
+<<<<<<< HEAD
 
                 
+=======
+>>>>>>> parent of 8cce8c7... CRUD_Silverlight
             }
         }
 
         protected void CreateUser_CreatedUser(object sender, EventArgs e)
         {
+<<<<<<< HEAD
+=======
+            successLabel.Text = "De user is toegevoegd.";
+>>>>>>> parent of 8cce8c7... CRUD_Silverlight
             string continueUrl =  "~/Admin/adminPage.aspx";
             Response.Redirect(continueUrl);
         }
 
+<<<<<<< HEAD
         protected void UserList_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
     }
+=======
+        protected void RoleList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String selectRole = RoleList.Text;
+            String[] allUsers = Roles.GetUsersInRole(selectRole);
+            UserRoleList.DataSource = allUsers.ToList();
+            UserRoleList.DataBind();
+        }
+
+    }
+
+
+>>>>>>> parent of 8cce8c7... CRUD_Silverlight
 }
